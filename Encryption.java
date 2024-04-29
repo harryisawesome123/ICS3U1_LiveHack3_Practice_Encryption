122131c48db72c6a2fec6d896bcf4e3893a8b33d
harryisawesome123 <harish.rahukulan25@ycdsbk12.ca>
2024-04-29 18:07:07 -0400

    hoho

--- Encryption.java
+++ Encryption.java
@@ -7,6 +7,28 @@ class Encryption extends ConsoleProgram {
   
   public void run() {
     
+    
+    String strMessage;
+    String resulted;
+
+    System.out.println("**** Encryption Program *****");
+
+    strMessage = readLine("Enter the message to encrypt: ");
+    int intnumber = readInt("Enter the shift amount: ");
+
+    resulted = EncryptionMethod(strMessage, intnumber);
+
+    System.out.println("Here is your encrypted message: " + resulted);
   }
 
+  public String EncryptionMethod(String str, int number) {
+
+    String result = "";
+
+    for (int i = 0; i < str.length(); i++) {
+      result += (char)(' ' + ((str.charAt(i) - ' ' + number) % 95));
+    }
+
+    return result;
+  }
 }
